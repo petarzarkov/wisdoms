557219bf6c56ac2db842653a252be8652bcfb593
petarzarkov <pzarko1@gmail.com>
2025-11-20 14:59:58 +0200

    fix: proper assets path

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
+import { writeFileSync } from 'fs';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
 
 export default defineConfig({
+  base: '/wisdoms/',
   root: 'src',
   build: {
     outDir: '../dist',
     assetsDir: 'assets',
     emptyOutDir: true,
   },
+  plugins: [
+    {
+      name: 'add-nojekyll',
+      closeBundle() {
+        writeFileSync(resolve(__dirname, 'dist', '.nojekyll'), '');
+      },
+    },
+  ],
 });
